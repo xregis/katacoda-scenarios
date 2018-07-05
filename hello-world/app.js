ceac588ef1081f1d19ae100c3c4755c23daadd82
Regis <xregiss@gmail.com>
2018-07-05 19:57:46 +0000

    ok

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,2 +1,17 @@
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+
+var serviceAccount = require('./admob-app-id-8755635507-89bd2394db7f.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+
+var docRef = db.collection('compras').doc('loja');
+
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
